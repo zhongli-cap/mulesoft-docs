ca450caf63dcb2c18d526db160b01bef1a50e6a0
Dan Allen <dan.j.allen@gmail.com>
2016-09-17 02:15:20 -0600

    update repository consolidation script
    - reword description
    - change PUBLISH_DIR variable to REPO_DIR

--- gradle/consolidate-publish-repo.sh
+++ gradle/consolidate-publish-repo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# This script consolidates the git history of the publish repository by
-# squashing commits older than the specified age into a new starting commit,
-# then restoring subsequent commits from that point forward.
+# This script consolidates the history (commits) of a git repository by
+# squashing commits older than the specified age into a new starting commit
+# and then restoring subsequent commits from that point forward.
 
 DRY_RUN=false
 QUIET_FLAG=
 MAX_AGE='3 week'
 FREQUENCY='1 month'
-PUBLISH_DIR=_publish
+REPO_DIR=_publish
 
 while getopts 'dqa:f:D:' option; do
   case $option in
@@ -16,11 +16,11 @@ while getopts 'dqa:f:D:' option; do
     q) QUIET_FLAG="--quiet" ;;
     a) MAX_AGE="$OPTARG" ;;
     f) FREQUENCY="$OPTARG" ;;
-    D) PUBLISH_DIR="$OPTARG" ;;
+    D) REPO_DIR="$OPTARG" ;;
   esac
 done
 
-cd $PUBLISH_DIR
+cd $REPO_DIR
 
 # NOTE using the GitHub API would allow us to start with a shallow repository
 # UNTIL_PARAM_VAL="$MAX_AGE $FREQUENCY"
@@ -28,9 +28,7 @@ cd $PUBLISH_DIR
 # if [ "0" == `curl -s https://api.github.com/repos/mulesoft/mulesoft-docs-build-output/commits?until=$UNTIL_PARAM_VAL -H "Accept: application/vnd.github.json" | grep -c sha` ]; then
 #   echo 'No commits to consolidate.'
 #   exit 0
-# fi
-#
-# if [ -f .git/shallow ]; then
+# elif [ -f .git/shallow ]; then
 #   git fetch --unshallow
 # fi
 
@@ -68,9 +66,10 @@ git gc $QUIET_FLAG --prune=now
 # TODO could check for BUILD_ID environment variable instead
 if [ "$DRY_RUN" == "false" ]; then
   git push $QUIET_FLAG --force origin master
+  # NOTE if the push fails, remove the clone as it's now stale
   if [ $? -ne 0 ]; then
     cd ..
-    rm -rf $PUBLISH_DIR
+    rm -rf $REPO_DIR
   fi
 fi
 
